3cd23d65b3c6b19782d8c7571ef6214ea5329428
Unknown2526 <an_tho_888@live.ca>
2018-02-02 12:28:54 -0500

    compléter le main()

--- TP1/src/CV.java
+++ TP1/src/CV.java
@@ -10,6 +10,28 @@ public class CV {
 	
 	public static void main(String[] args) {
 		
+		CV cv = new CV();
+		cv.nom = "Chow";
+		cv.prenom = "Anthony";
+		cv.formation = "Technique de l'informatique";
+		cv.nbAnnee = 2;
+		cv.competence[0] = "Coder";
+		cv.competence[1] = "Décoder";
+		cv.attente = "Apprendre à bien maîtriser le GIT.";
+		
+		CV cv2 = new CV();
+		cv2.nom = "Chrisphonte";
+		cv2.prenom = "Vladimir";
+		cv2.formation = "Technique de l'informatique";
+		cv2.nbAnnee = 2;
+		cv2.competence[0] = "Base de données";
+		cv2.competence[1] = "Identification";
+		cv2.attente = "Apprendre à bien maîtriser le GIT.";
+		
+		System.out.println("Bienvenue chez Barette!");
+		
+		cv.affiche();
+		cv2.affiche();
 	}
 
 	void affiche() {
