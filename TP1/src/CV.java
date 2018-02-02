e8f34b575e4905359b561ca614181f8341feb0f7
Unknown2526 <an_tho_888@live.ca>
2018-02-02 12:27:02 -0500

    CV - Original

---
+++ TP1/src/CV.java
@@ -0,0 +1,16 @@
+
+public class CV {
+
+	private String nom;
+	private String prenom;
+	private String formation;
+	private int nbAnnee;
+	private String[] competence = new String[2];
+	private String attente;
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
+
